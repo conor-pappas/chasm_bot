441a37d2514877948c340ed0bc60fbe31e593194
Conor Pappas <conor.pappas@gmail.com>
2016-04-11 17:42:57 -0400

    Fixes ticketDescription for calling a revote

--- app/value_objects/slash_actions/Revote.scala
+++ app/value_objects/slash_actions/Revote.scala
@@ -24,7 +24,7 @@ object Revote extends SlashAction {
   }
 
   def sendRevoteCalledMessage(ticketDescription:String) = {
-    val message = s"<!here|here> Given the discussion above about #ticketDescription, " + 
+    val message = s"<!here|here> Given the discussion above about $ticketDescription, " + 
     "please place a new vote using `/chasm vote`. Maybe try coming to a " + 
     "consensus this time so I can take a break."
     slack.IncomingWebhookClient.postInChannel(message)   
